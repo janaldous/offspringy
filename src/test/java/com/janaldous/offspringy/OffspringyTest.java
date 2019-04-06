6e0508f1afa113a96c1030f61ec1a6fd1aca01f2
janaldoustorres <jat.torres@gmail.com>
2019-04-07 00:09:19 +0800

    fixed tests

--- src/test/java/com/janaldous/offspringy/OffspringyTest.java
+++ src/test/java/com/janaldous/offspringy/OffspringyTest.java
@@ -1,10 +1,5 @@
 package com.janaldous.offspringy;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public abstract class OffspringyTest {
